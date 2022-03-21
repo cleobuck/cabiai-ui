58063d507b45a3f9d2d1755d1fb3c15d25eb68c3
cleobuck <cleo.buck@entaingroup.com>
2022-03-21 09:08:01 +0100

    fixed range color issue

--- src/stories/components/range-button/RangeButton.js
+++ src/stories/components/range-button/RangeButton.js
@@ -1,48 +1,33 @@
 import "./RangeButton.scss";
 
-import React, { useRef, useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import PropTypes from "prop-types";
 
 export default function RangeButton({ min, max, onChange }) {
   const [minVal, setMinVal] = useState(min);
   const [maxVal, setMaxVal] = useState(max);
 
-  const minValRef = useRef(null);
-  const maxValRef = useRef(null);
-  const range = useRef(null);
+  const [rangeDimensions, setRangeDimension] = useState({
+    width: "100%",
+    left: "0%",
+  });
 
   // Convert to percentage
   const getPercent = useCallback(
-    (value) => {
-      Math.round(((value - min) / (max - min)) * 100);
-    },
+    (value) => Math.round(((value - min) / (max - min)) * 100),
     [min, max]
   );
 
   // Set width of the range to decrease from the left side
   useEffect(() => {
-    if (maxValRef.current) {
-      const minPercent = getPercent(minVal);
-      const maxPercent = getPercent(+maxValRef.current.value);
+    const minPercent = getPercent(minVal);
+    const maxPercent = getPercent(maxVal);
 
-      if (range.current) {
-        range.current.style.left = `${minPercent}%`;
-        range.current.style.width = `${maxPercent - minPercent}%`;
-      }
-    }
-  }, [minVal, getPercent]);
-
-  // Set width of the range to decrease from the right side
-  useEffect(() => {
-    if (minValRef.current) {
-      const minPercent = getPercent(+minValRef.current.value);
-      const maxPercent = getPercent(maxVal);
-
-      if (range.current) {
-        range.current.style.width = `${maxPercent - minPercent}%`;
-      }
-    }
-  }, [maxVal, getPercent]);
+    setRangeDimension({
+      width: `${maxPercent - minPercent}%`,
+      left: `${minPercent}%`,
+    });
+  }, [minVal, maxVal, getPercent]);
 
   // Get min and max values when their state changes
   useEffect(() => {
@@ -57,12 +42,9 @@ export default function RangeButton({ min, max, onChange }) {
         min="0"
         max="1000"
         value={minVal}
-        ref={minValRef}
-        onChange={(event) => {
-          const value = Math.min(+event.target.value, maxVal - 1);
-          setMinVal(value);
-          event.target.value = value.toString();
-        }}
+        onChange={(event) =>
+          setMinVal(Math.min(+event.target.value, maxVal - 1))
+        }
         className={`thumb thumb--zindex-${minVal > max - 100 ? "5" : "3"}`}
       />
       <input
@@ -70,17 +52,17 @@ export default function RangeButton({ min, max, onChange }) {
         min="0"
         max="1000"
         value={maxVal}
-        ref={maxValRef}
         onChange={(event) => {
-          const value = Math.max(+event.target.value, minVal + 1);
-          setMaxVal(value);
-          event.target.value = value.toString();
+          setMaxVal(Math.max(+event.target.value, minVal + 1));
         }}
         className="thumb thumb--zindex-4"
       />
       <div className="slider">
         <div className="slider__track" />
-        <div ref={range} className="slider__range" />
+        <div
+          style={{ width: rangeDimensions.width, left: rangeDimensions.left }}
+          className="slider__range"
+        />
         <div className="slider__left-value">{minVal}</div>
         <div className="slider__right-value">{maxVal}</div>
       </div>
