6b6902f4a5a0221f7a4b16ee047a1d7814bf9dac
cleobuck <cleo.buck@entaingroup.com>
2022-03-23 16:30:25 +0100

    finished card

--- src/stories/components/post-card/PostCard.stories.jsx
+++ src/stories/components/post-card/PostCard.stories.jsx
@@ -1,42 +1,50 @@
 import PostCard from "./PostCard";
 
-export default  {
-  title: "components/cards/post",
-  component: PostCard
-}
+export default {
+	title: "components/cards/post",
+	component: PostCard,
+};
 
-const Template = args => <PostCard {...args} />
+const Template = (args) => <PostCard {...args} />;
 
-export const Default = Template.bind({})
-
+export const Default = Template.bind({});
 
 Default.args = {
-  data: {
-    endDate: "01/31/2023",
-    startDate: "01/01/2023",
-    title: "Tour d’Europe du nord en voiture",
-    countryList: ["Islande", "SriLanka", "Perou", "Italie", "Afghanistan"],
-    flexibleDate: true
+	data: {
+		endDate: "01/31/2023",
+		startDate: "01/01/2023",
+		title: "Tour d’Europe du nord en voiture",
+		countryList: ["Islande", "SriLanka", "Perou", "Italie", "Afghanistan"],
+		flexibleDate: true,
+	},
+	favorite: false,
+	// unavailable: true
+};
 
-  },
-  favorite: false,
-  // unavailable: true
-  
-}
-
-export const Favorite = Template.bind({})
-
+export const Favorite = Template.bind({});
 
 Favorite.args = {
-  data: {
-    endDate: "01/31/2023",
-    startDate: "01/01/2023",
-    title: "Tour d’Europe du nord en voiture",
-    countryList: ["Islande", "SriLanka", "Perou", "Italie", "Afghanistan"],
-    flexibleDate: true
+	data: {
+		endDate: "01/31/2023",
+		startDate: "01/01/2023",
+		title: "Tour d’Europe du nord en voiture",
+		countryList: ["Islande", "SriLanka", "Perou", "Italie", "Afghanistan"],
+		flexibleDate: true,
+	},
+	favorite: true,
+	// unavailable: true
+};
 
-  },
-  favorite: true,
-  // unavailable: true
-  
-}
+export const Unavailable = Template.bind({});
+
+Unavailable.args = {
+	data: {
+		endDate: "01/31/2023",
+		startDate: "01/01/2023",
+		title: "Tour d’Europe du nord en voiture",
+		countryList: ["Islande", "SriLanka", "Perou", "Italie", "Afghanistan"],
+		flexibleDate: true,
+	},
+	favorite: true,
+	unavailable: true,
+};
