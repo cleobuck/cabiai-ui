16375e092f68b3cef1e933d1a690df09ba1b7d0d
cleobuck <cleo.buck@entaingroup.com>
2022-03-24 16:35:15 +0100

    vectors

---
+++ src/stories/assets/svg/less-button/LessButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LessButton({ className }) {
+	return (
+		<svg viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
+			<circle />
+			<rect />
+		</svg>
+	);
+}
---
+++ src/stories/assets/svg/more-button/MoreButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SuccessIcon({ className }) {
+	return (
+		<svg
+			className={className}
+			viewBox="0 0 36 36"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<circle />
+			<rect />
+			<rect />
+		</svg>
+	);
+}
