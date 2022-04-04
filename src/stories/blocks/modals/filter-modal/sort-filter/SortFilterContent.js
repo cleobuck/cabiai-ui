5c94e170256aea62b1a76d2842d39c452f356d95
cleobuck <cleo.buck@entaingroup.com>
2022-04-04 19:46:43 +0200

    text inputs

---
+++ src/stories/blocks/modals/filter-modal/sort-filter/SortFilterContent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import RadioButton from "stories/components/inputs/radio-button/RadioButton";
+import CheckBox from "stories/components/inputs/checkbox/CheckBox";
+
+export default function SortContent({ options, type }) {
+	return (
+		<ul className={`modal-sorting-list ${type === "filter" ? "-filter" : ""}`}>
+			{options.map((sortOption) => (
+				<li key={sortOption.id}>
+					<span>{sortOption.label} </span>
+					{type === "sort" && <RadioButton checked={sortOption.checked} />}
+					{type === "filter" && <CheckBox checked={sortOption.checked} />}
+				</li>
+			))}
+		</ul>
+	);
+}
---
+++ src/stories/components/text-inputs/select/Select.stories.js
@@ -0,0 +1,10 @@
+import Select from "./Select";
+
+export default {
+	title: "components/text inputs/select",
+	component: Select,
+};
+
+const Template = (args) => <Select />;
+
+export const unSelected = Template.bind({});
